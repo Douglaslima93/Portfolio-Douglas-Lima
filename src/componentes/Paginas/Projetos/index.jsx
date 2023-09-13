70adf4f8a9210d34826454c79c81902553d5b8ed
Douglas Lima <douglas07a@gmail.com>
2023-09-13 16:27:33 -0300

    Criando a pagina projeto junto com os Estilos

--- src/componentes/Paginas/Projetos/index.jsx
+++ src/componentes/Paginas/Projetos/index.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import './style.css'
 import EstruturaPagina from "../../EstruturaPagina";
+import { Link } from "react-router-dom";
+import deploy from "../../../assets/shuttle.png";
+import imagemProjeto1 from "../../../assets/imagem1.jpg";
+
 
 
 export default function Projetos() {
     return(
         <EstruturaPagina>
-        <section>
+        <section className="container-card">
         
-            Projetos
+            <div className="card">
+                <img src={imagemProjeto1} rel="imagem-do-projeto"/>
+
+                <div className="container-icon">
+
+                <Link className="icon" to="https://github.com/Douglaslima93/pousada-secreta" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" 
+                    rel="icone-git-hub"/>
+                </Link>
+                
+                <Link className="icon" to="https://pousadasecreta-angradosreis.netlify.app/" target="_blank">
+                <img src={deploy} rel="Deploy"/></Link>
+            </div>
+
+            </div>
         </section>
         </EstruturaPagina>
     )
