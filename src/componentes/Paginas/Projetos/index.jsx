e698de8ccac2fcea08f258b7444093d85eec6083
Douglas Lima <douglas07a@gmail.com>
2023-09-14 10:36:30 -0300

    Componentes da pagina projetos

--- src/componentes/Paginas/Projetos/index.jsx
+++ src/componentes/Paginas/Projetos/index.jsx
@@ -4,6 +4,12 @@ import EstruturaPagina from "../../EstruturaPagina";
 import { Link } from "react-router-dom";
 import deploy from "../../../assets/shuttle.png";
 import imagemProjeto1 from "../../../assets/imagem1.jpg";
+import imagemProjeto2 from "../../../assets/imagem2.jpg";
+import imagemProjeto3 from "../../../assets/imagem3.jpg";
+import imagemProjeto4 from "../../../assets/imagem4.jpg";
+import imagemProjeto5 from "../../../assets/imagem5.jpg";
+import imagemProjeto6 from "../../../assets/imagem6.jpg";
+
 
 
 
@@ -25,8 +31,83 @@ export default function Projetos() {
                 <Link className="icon" to="https://pousadasecreta-angradosreis.netlify.app/" target="_blank">
                 <img src={deploy} rel="Deploy"/></Link>
             </div>
-
             </div>
+
+            <div className="card">
+                <img src={imagemProjeto2} rel="imagem-do-projeto"/>
+
+                <div className="container-icon">
+
+                <Link className="icon" to="https://github.com/Douglaslima93/agencia-branding" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" 
+                    rel="icone-git-hub"/>
+                </Link>
+                
+                <Link className="icon" to="https://agencia-branding.netlify.app/" target="_blank">
+                <img src={deploy} rel="Deploy"/></Link>
+            </div>
+            </div>
+
+            <div className="card">
+                <img src={imagemProjeto3} rel="imagem-do-projeto"/>
+
+                <div className="container-icon">
+
+                <Link className="icon" to="https://github.com/Douglaslima93/Consultorio-Dentes_Saudaveis" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" 
+                    rel="icone-git-hub"/>
+                </Link>
+                
+                <Link className="icon" to="https://consultorio-dentes-saudaveis.netlify.app/" target="_blank">
+                <img src={deploy} rel="Deploy"/></Link>
+            </div>
+            </div>
+
+            <div className="card">
+                <img src={imagemProjeto4} rel="imagem-do-projeto"/>
+
+                <div className="container-icon">
+
+                <Link className="icon" to="https://github.com/Douglaslima93/signos" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" 
+                    rel="icone-git-hub"/>
+                </Link>
+                
+                <Link className="icon" to="https://card-de-signos.netlify.app/" target="_blank">
+                <img src={deploy} rel="Deploy"/></Link>
+            </div>
+            </div>
+
+            <div className="card">
+                <img src={imagemProjeto5} rel="imagem-do-projeto"/>
+
+                <div className="container-icon">
+
+                <Link className="icon" to="https://github.com/Douglaslima93/Barbershop" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" 
+                    rel="icone-git-hub"/>
+                </Link>
+                
+                <Link className="icon" to="https://barber-shop-23.netlify.app/" target="_blank">
+                <img src={deploy} rel="Deploy"/></Link>
+            </div>
+            </div>
+
+            <div className="card">
+                <img src={imagemProjeto6} rel="imagem-do-projeto"/>
+
+                <div className="container-icon">
+
+                <Link className="icon" to="https://github.com/Douglaslima93/Gelateria" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" 
+                    rel="icone-git-hub"/>
+                </Link>
+                
+                <Link className="icon" to="https://gelateria-tropical.netlify.app/" target="_blank">
+                <img src={deploy} rel="Deploy"/></Link>
+            </div>
+            </div>
+
         </section>
         </EstruturaPagina>
     )
